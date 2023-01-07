e1d000c72b62f498b95b19fdaa1bb084de9a7a03
Josue-dev <52864843+Josue-dev@users.noreply.github.com>
2023-01-07 12:25:52 -0600

    Configurando rutas para carga perezosa

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,10 +2,7 @@
 import { ellipse, square, triangle } from 'ionicons/icons';
 import { lazy,LazyExoticComponent} from 'react';
 
-// import Home from "../pages/Home";
-// import Tab1 from '../pages/Tab1';
-// import Tab2 from "../pages/Tab2";
-// import Tab3 from "../pages/Tab3";
+
 
 type JSXComponent= React.FC<{}>;
 
@@ -17,33 +14,29 @@ interface Route{
     icon:string;
 }
 
-const Tab1=lazy(()=>import(/*webpackChunkName:"Tab1"*/'../pages/Tab1'));
-const Tab2=lazy(()=>import(/*webpackChunkName:"Tab2"*/'../pages/Tab2'));
-const Tab3=lazy(()=>import(/*webpackChunkName:"Tab3"*/'../pages/Tab3'));
-const Home=lazy(()=>import(/*webpackChunkName:"Home"*/'../pages/Home'));
 
 export const routes:Route[]=[
     {
-        path:'/tab1',
-        Component:Tab1,
+        path:'/home',
+        Component:lazy(()=>import(/*webpackChunkName:"LazyLayout"*/'../pages/Home')),
         name:'Home',
         icon:triangle
     },
     {
-        path:'/tab2',
-        Component:Tab2,
+        path:'/tab1',
+        Component:lazy(()=>import(/*webpackChunkName:"LazyLayout"*/'../pages/Tab1')),
         name:'Inicio',
         icon:square
     },
     {
-        path:'/tab3',
-        Component:Tab3,
+        path:'/tab2',
+        Component:lazy(()=>import(/*webpackChunkName:"LazyLayout"*/'../pages/Tab2')),
         name:'Servicios',
         icon:ellipse
     },
     {
-        path:'/home',
-        Component:Home,
+        path:'/tab3',
+        Component:lazy(()=>import(/*webpackChunkName:"LazyLayout"*/'../pages/Tab3')),
         name:'Favoritos',
         icon:ellipse
     }
